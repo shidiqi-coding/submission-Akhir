dc28a69c3646fd314970cb65fa8301bd6394cbab
shidiqi-coding <shidiqiihsan@gmail.com>
2024-07-29 21:21:23 +0700

    Create main.js

---
+++ src/main.js
@@ -0,0 +1,18 @@
+ import noteComponent from "../src/component/list-item.js";
+import "../src/script/view/my-footer.js";
+import "./script/view/input-form.js"
+import "./script/form-title.js";
+import apps from './styles/apps.js';
+
+document.addEventListener('DOMContentLoaded', () =>{
+  customElements.define("note-list",noteComponent);
+
+});
+
+apps();
+//document.querySelector('.btn').className = 'fa fa-star';
+
+//document.addEventListener('DOMContentLoaded', () =>{
+//  customElements.define("my-footer",myFooter);
+
+//});
